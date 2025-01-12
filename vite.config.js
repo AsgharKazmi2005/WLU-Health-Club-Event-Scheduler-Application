2b43097e4515223c08059a9c1edc0b8ad95b61b2
AsgharKazmi2005 <asgharkazmi05@gmail.com>
2025-01-12 15:22:28 -0500

    Added fontawesome
    Added fontawesome to vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@fortawesome/react-fontawesome"],
+  },
   build: {
     rollupOptions: {
       external: ["react-big-calendar"],
