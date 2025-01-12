a28ace6a8470ca7f9ffe7fe396012ac3e6831b9b
AsgharKazmi2005 <asgharkazmi05@gmail.com>
2025-01-12 15:38:31 -0500

    Uncommented Moment Library

--- src/components/WeekViewCalendar.jsx
+++ src/components/WeekViewCalendar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
-// import moment from "moment";
+import moment from "moment";
 import "../calendar.css";
 import { supabase } from "../supabaseClient";
 
