6632f321360c92ddb322ba370bf1437908aa8ac4
AsgharKazmi2005 <asgharkazmi05@gmail.com>
2025-01-12 15:12:36 -0500

    Added calendar.css
    Copied react-big-calendar library as a local file

--- src/components/WeekViewCalendar.jsx
+++ src/components/WeekViewCalendar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../calendar.css";
 import { supabase } from "../supabaseClient";
 
 const localizer = momentLocalizer(moment);
