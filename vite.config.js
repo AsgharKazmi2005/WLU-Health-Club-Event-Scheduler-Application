dbba686265d4e64b1f776ea4be1ad3a8762aa8e3
AsgharKazmi2005 <asgharkazmi05@gmail.com>
2025-01-12 23:00:30 -0500

    Removed rollups

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-big-calendar",
-        "@fortawesome/react-fontawesome",
-        "moment",
-      ],
-    },
-  },
 });
