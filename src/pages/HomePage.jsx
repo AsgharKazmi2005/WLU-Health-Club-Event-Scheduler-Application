9e8a5f1984bb226c5336077c757bf3c9323fe6ce
AsgharKazmi2005 <107439099+AsgharKazmi2005@users.noreply.github.com>
2025-02-23 19:40:58 -0500

    Added React-gallery feature
    Utilized the react image gallery library to add a beautiful gallery showing an event.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,13 +9,26 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+import img1 from "../images/CPRClass/CPRClass/DSC03608.jpg";
+import img2 from "../images/CPRClass/CPRClass/DSC03610.jpg";
+import img3 from "../images/CPRClass/CPRClass/DSC03611.jpg";
+import img4 from "../images/CPRClass/CPRClass/DSC03613.jpg";
+import img5 from "../images/CPRClass/CPRClass/DSC03619.jpg";
+import img6 from "../images/CPRClass/CPRClass/DSC03621.jpg";
+import img7 from "../images/CPRClass/CPRClass/DSC03625.jpg";
+import img8 from "../images/CPRClass/CPRClass/DSC03628.jpg";
+import img9 from "../images/CPRClass/CPRClass/DSC03633.jpg";
+import img10 from "../images/CPRClass/CPRClass/DSC03636.jpg";
 
 const HomePage = () => {
   const [events, setEvents] = useState([]);
   const [selectedDate, setSelectedDate] = useState(dayjs());
   const [eventsOnSelectedDate, setEventsOnSelectedDate] = useState([]);
+  const [images, setImages] = useState([]);
 
-  // Fetch events from Supabase
   useEffect(() => {
     const fetchEvents = async () => {
       const { data, error } = await supabase
@@ -33,7 +46,6 @@ const HomePage = () => {
     fetchEvents();
   }, []);
 
-  // Update events for the selected date
   useEffect(() => {
     const eventsForDate = events.filter((event) =>
       dayjs(event.start_time).isSame(selectedDate, "day")
@@ -41,14 +53,27 @@ const HomePage = () => {
     setEventsOnSelectedDate(eventsForDate);
   }, [selectedDate, events]);
 
+  useEffect(() => {
+    setImages([
+      { original: img1, thumbnail: img1 },
+      { original: img2, thumbnail: img2 },
+      { original: img3, thumbnail: img3 },
+      { original: img4, thumbnail: img4 },
+      { original: img5, thumbnail: img5 },
+      { original: img6, thumbnail: img6 },
+      { original: img7, thumbnail: img7 },
+      { original: img8, thumbnail: img8 },
+      { original: img9, thumbnail: img9 },
+      { original: img10, thumbnail: img10 },
+    ]);
+  }, []);
+
   const ServerDay = (props) => {
     const { day, outsideCurrentMonth, ...other } = props;
-  
-    // Check if this day has an event
     const hasEvent = events.some((event) =>
       dayjs(event.start_time).isSame(day, "day")
     );
-  
+
     return (
       <Badge
         key={day.toString()}
@@ -56,9 +81,9 @@ const HomePage = () => {
         badgeContent={hasEvent ? "💞" : undefined}
         sx={{
           "& .MuiBadge-badge": {
-            backgroundColor: "transparent", // Removes blue background
-            color: "inherit", // Ensures emoji keeps its natural color
-            fontSize: ".8rem", // Adjusts emoji size if needed
+            backgroundColor: "transparent",
+            color: "inherit",
+            fontSize: ".8rem",
           },
         }}
       >
@@ -72,43 +97,55 @@ const HomePage = () => {
       <Navbar />
       <div className="home-page">
         <div className="home-center">
-          <div className="site-development-message">
-            <h1>Site in Development</h1>
-            <p>Check back later for updates!</p>
+          <div className="image-gallery-container">
+          <ImageGallery
+            items={images}
+            autoPlay={true}             // Enables autoplay
+            slideInterval={5000}        // Slide changes every 3 seconds
+            slideDuration={1000}        // Slide transition duration is 1 second
+            infinite={true}             // Infinite looping
+            showPlayButton={true}       // Show play/pause button
+            showFullscreenButton={true} // Show fullscreen button
+            showThumbnails={true}       // Show thumbnails
+            thumbnailPosition="left"
+            lazyLoad={true}
+            showNav={true}
+            showBullets={true}
+          />
+
           </div>
           <div className="home-cal">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar
-              showDaysOutsideCurrentMonth
-              fixedWeekNumber={6}
-              value={selectedDate}
-              onChange={(newValue) => setSelectedDate(newValue)}
-              slots={{ day: ServerDay }}
-              slotProps={{
-                day: {
-                  sx: {
-                    "&.Mui-selected": {
-                      backgroundColor: "#692f2feb", // Selected date color
-                      color: "white",
-                    },
-                    "&.Mui-selected:hover": {
-                      backgroundColor: "#2b0000", // Hover color
-                    },
-                    "&.Mui-focusVisible": {
-                      backgroundColor: "#2b0000", // Change focus color
-                      color: "white",
-                      outline: "none",
+              <DateCalendar
+                showDaysOutsideCurrentMonth
+                fixedWeekNumber={6}
+                value={selectedDate}
+                onChange={(newValue) => setSelectedDate(newValue)}
+                slots={{ day: ServerDay }}
+                slotProps={{
+                  day: {
+                    sx: {
+                      "&.Mui-selected": {
+                        backgroundColor: "#692f2feb",
+                        color: "white",
+                      },
+                      "&.Mui-selected:hover": {
+                        backgroundColor: "#2b0000",
+                      },
+                      "&.Mui-focusVisible": {
+                        backgroundColor: "#2b0000",
+                        color: "white",
+                        outline: "none",
+                      },
                     },
                   },
-                },
-              }}
-              sx={{
-                backgroundColor: "#ee8888",
-                borderRadius: "10px",
-                padding: "10px",
-              }}
+                }}
+                sx={{
+                  backgroundColor: "#ee8888",
+                  borderRadius: "10px",
+                  padding: "10px",
+                }}
               />
-
             </LocalizationProvider>
             <div className="event-list">
               <h3>Events on {selectedDate.format("MMMM D, YYYY")}</h3>
