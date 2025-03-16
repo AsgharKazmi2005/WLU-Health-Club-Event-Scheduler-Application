61f2c63ae7f2a0384dcbc04aaa6c58e674519bb6
AsgharKazmi2005 <107439099+AsgharKazmi2005@users.noreply.github.com>
2025-03-16 13:19:20 -0400

    Adjusted Mobile Style
    Test was clipping off on mobile screen. Added a fix to permute the div dimensions depending on the screen size.

--- src/components/Portrait.jsx
+++ src/components/Portrait.jsx
@@ -1,11 +1,9 @@
 function Portrait({ image, name, description }) {
   return (
-    <div className="prt flex flex-col items-center bg-[#1a1a1a] p-4 rounded-lg shadow-lg hover:shadow-xl hover:scale-[1.02] border border-transparent hover:border-[rgba(255,145,145,0.95)] transition-all duration-700 h-[320px]">
+    <div className="prt flex flex-col items-center bg-[#1a1a1a] p-4 rounded-lg shadow-lg hover:shadow-xl hover:scale-[1.02] border border-transparent hover:border-[rgba(255,145,145,0.95)] transition-all duration-700 h-full">
       <img src={image} alt={name} className="w-60 h-40 object-cover rounded-lg mb-3" />
       <h3 className="text-xl font-semibold text-[rgba(255,145,145,0.95)]">{name}</h3>
-      <p className="text-white text-sm text-center flex-grow overflow-hidden text-ellipsis line-clamp-4">
-        {description}
-      </p>
+      <p className="text-white text-sm text-center flex-grow">{description}</p>
     </div>
   );
 }
