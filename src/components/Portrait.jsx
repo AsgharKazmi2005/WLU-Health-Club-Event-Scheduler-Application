3f7aa543f95182cef5e06b2c56d695480df3779e
AsgharKazmi2005 <107439099+AsgharKazmi2005@users.noreply.github.com>
2025-11-09 16:45:30 -0500

    update portrait component

--- src/components/Portrait.jsx
+++ src/components/Portrait.jsx
@@ -1,8 +1,15 @@
-function Portrait({ image, name, description }) {
+function Portrait({ image, name, position, description }) {
   return (
     <div className="prt flex flex-col items-center bg-[#1a1a1a] p-4 rounded-lg shadow-lg hover:shadow-xl hover:scale-[1.02] border border-transparent hover:border-[rgba(255,145,145,0.95)] transition-all duration-700 h-full">
-      <img src={image} alt={name} className="w-60 h-40 object-cover rounded-lg mb-3" />
-      <h3 className="text-xl font-semibold text-[rgba(255,145,145,0.95)]">{name}</h3>
+      <img
+        src={image}
+        alt={name}
+        className="w-60 h-40 object-cover rounded-lg mb-3"
+      />
+      <h3 className="text-xl font-semibold text-[rgba(255,145,145,0.95)]">
+        {name}
+      </h3>
+      <p className="text-gray-400 text-sm italic mb-2">{position}</p>
       <p className="text-white text-sm text-center flex-grow">{description}</p>
     </div>
   );
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -8,44 +8,116 @@ import Gabe from "../images/Gabe.webp";
 import David from "../images/David.webp";
 import Elizabeth from "../images/elizabeth.png";
 import Shaylin from "../images/shaylin.png";
-
-
+// import Clara from "../images/clara.png"; 
 
 function ContactPage() {
   return (
     <>
       <Navbar />
-              {/* Team Section */}
-        <div className="portraits">
+
+      {/* Team Section */}
+      <div className="portraits">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-6 mt-12 rightsize">
-          <Portrait image={Daniel} name="Daniel Volodarsky" description="Hey my name is Daniel Volodarsky and I am the Captain and Founder of the AHA Heart Club here at Washington and Lee University. I am a sophomore Biology and Music major on the pre-med track and am also proud to be an AHA licensed CPR Instructor. I love to talk to my friends, play violin, and study botany in my free time!" />
-          <Portrait image={David} name="David Shepherd" description="My name is David Shepherd, and I’m the Vice President of the AHA Heart Club at Washington and Lee University. I’m a sophomore Neuroscience major looking for ways to improve my community, so I’m extremely grateful for the opportunity to spread health consciousness and skills to the people of Lexington, VA. When I’m not working, I like to draw, read, and explore new cultural experiences!" />
-          <Portrait image={Bariz} name="Bariz Sultankhil" description="Hi, I'm Bariz Sultankhil! I live in Denver, Colorado, and I'm a neuroscience major on the pre-med track. I have a strong passion for research and healthcare.Outside of academics, I enjoy hiking, playing soccer, drawing, and learning new languages. I'm always excited to connect with others who share similar interests!" />
-          <Portrait image={Elizabeth} name="Elizabeth Sawicki" description="My name is Elizabeth Sawicki! I am in the class of 2029, majoring in Biology on a pre-veterinary track. I love being on the Women's Track and Field Team, I am a Hurdler and Triple Jumper." />
-          <Portrait image={Shaylin} name="Shaylin Ainsworth" description="My name is Shaylin Ainsworth! I am a neuroscience major, class of 2028 and I enjoy playing the violin!" />
-          <Portrait image={Gabe} name="Gabe Castillo" description="My name is Gabe Castillo and I’m the CPR Instruction Chair of the AHA Heart Club here at Washington and Lee University. I’m a first-year studying Biology on the pre-med track. I’m passionate about global health equity and serving others through healthcare. I love exploring music, weightlifting, and playing piano." />
+
+          <Portrait
+            image={Daniel}
+            name="Daniel Volodarsky"
+            position="Founder and President"
+            description="Hey my name is Daniel Volodarsky and I am the Captain and Founder of the AHA Heart Club here at Washington and Lee University. I am a sophomore Biology and Music major on the pre-med track and am also proud to be an AHA licensed CPR Instructor. I love to talk to my friends, play violin, and study botany in my free time!"
+          />
+
+          <Portrait
+            image={David}
+            name="David Shepherd"
+            position="Vice President"
+            description="My name is David Shepherd, and I’m the Vice President of the AHA Heart Club at Washington and Lee University. I’m a sophomore Neuroscience major looking for ways to improve my community, so I’m extremely grateful for the opportunity to spread health consciousness and skills to the people of Lexington, VA. When I’m not working, I like to draw, read, and explore new cultural experiences!"
+          />
+
+          <Portrait
+            image={Elizabeth}
+            name="Elizabeth Sawicki"
+            position="Treasurer"
+            description="My name is Elizabeth Sawicki! I am in the class of 2029, majoring in Biology on a pre-veterinary track. I love being on the Women's Track and Field Team, I am a Hurdler and Triple Jumper."
+          />
+
+          <Portrait
+            image={Bariz}
+            name="Bariz Sultankhil"
+            position="Assistant Treasurer"
+            description="Hi, I'm Bariz Sultankhil! I live in Denver, Colorado, and I'm a neuroscience major on the pre-med track. I have a strong passion for research and healthcare. Outside of academics, I enjoy hiking, playing soccer, drawing, and learning new languages. I'm always excited to connect with others who share similar interests!"
+          />
+
+          <Portrait
+            image={Gabe}
+            name="Gabe Castillo"
+            position="CPR Instruction Chair"
+            description="My name is Gabe Castillo and I’m the CPR Instruction Chair of the AHA Heart Club here at Washington and Lee University. I’m a first-year studying Biology on the pre-med track. I’m passionate about global health equity and serving others through healthcare. I love exploring music, weightlifting, and playing piano."
+          />
+
+          {/* <Portrait
+            image={Clara}
+            name="Clara Avery"
+            position="Social Chair"
+            description="Hi! I’m Clara Avery, the Social Chair of the AHA Heart Club. I’m passionate about connecting people and building community through our events and outreach."
+          /> */}
+
+          <Portrait
+            image={Shaylin}
+            name="Shaylin Ainsworth"
+            position="Secretary"
+            description="My name is Shaylin Ainsworth! I am a neuroscience major, class of 2028 and I enjoy playing the violin!"
+          />
+
         </div>
-        </div>
-<div className="calendar-main flex flex-col items-center min-h-screen bg-[#402121]/95 text-white">
-      <div className="contact-form w-[60%] bg-[#1a1a1a] shadow-lg rounded-lg p-8 mt-10 hover:shadow-xl hover:scale-[1.02] border border-transparent hover:border-[rgba(255,145,145,0.95)]  transition-all duration-700">
-      <h2 className="text-3xl font-bold text-center mb-6 text-[rgba(255,145,145,0.95)]">Contact Us</h2>
-          <form action="https://formsubmit.co/7da7c43e1f234e2e51c7abad9ea8c367" method="POST" className="space-y-4">
+      </div>
+
+      {/* Contact Form */}
+      <div className="calendar-main flex flex-col items-center min-h-screen bg-[#402121]/95 text-white">
+        <div className="contact-form w-[60%] bg-[#1a1a1a] shadow-lg rounded-lg p-8 mt-10 hover:shadow-xl hover:scale-[1.02] border border-transparent hover:border-[rgba(255,145,145,0.95)] transition-all duration-700">
+          <h2 className="text-3xl font-bold text-center mb-6 text-[rgba(255,145,145,0.95)]">
+            Contact Us
+          </h2>
+          <form
+            action="https://formsubmit.co/7da7c43e1f234e2e51c7abad9ea8c367"
+            method="POST"
+            className="space-y-4"
+          >
             <input type="hidden" name="_subject" value="New Contact Form Submission" />
             <input type="hidden" name="_captcha" value="false" />
-            
+
             <label className="block">Name:</label>
-            <input type="text" name="name" required className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]" />
-            
+            <input
+              type="text"
+              name="name"
+              required
+              className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]"
+            />
+
             <label className="block">Email:</label>
-            <input type="email" name="email" required className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]" />
-            
+            <input
+              type="email"
+              name="email"
+              required
+              className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]"
+            />
+
             <label className="block">Message:</label>
-            <textarea name="message" required className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white h-32 focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]"></textarea>
-            
-            <button type="submit" className="w-full bg-[rgba(255,145,145,0.95)] text-white py-2 rounded-lg hover:bg-[rgba(255,145,145,1)] transition">Send</button>
+            <textarea
+              name="message"
+              required
+              className="w-full p-2 border border-[#333] rounded-lg bg-[#121212] text-white h-32 focus:ring-2 focus:ring-[rgba(255,145,145,0.95)]"
+            ></textarea>
+
+            <button
+              type="submit"
+              className="w-full bg-[rgba(255,145,145,0.95)] text-white py-2 rounded-lg hover:bg-[rgba(255,145,145,1)] transition"
+            >
+              Send
+            </button>
           </form>
         </div>
       </div>
+
       <Footer />
     </>
   );
